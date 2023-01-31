45ae7f3884f49649dcb045d1a056ac89139d6d0d
Nando <24811313+fzavalia@users.noreply.github.com>
2023-01-31 16:32:51 -0300

    chore: Test fromCollectionFragment

--- src/ports/collections/utils.spec.ts
+++ src/ports/collections/utils.spec.ts
@@ -1,5 +1,6 @@
-import { CollectionSortBy } from '@dcl/schemas'
-import { getCollectionsQuery } from './utils'
+import { ChainId, CollectionSortBy, Network } from '@dcl/schemas'
+import { CollectionFragment } from './types'
+import { fromCollectionFragment, getCollectionsQuery } from './utils'
 
 describe('#getCollectionsQuery', () => {
   describe('when sortBy is CollectionSortBy.RECENTLY_LISTED', () => {
@@ -25,6 +26,57 @@ describe('#getCollectionsQuery', () => {
       })
 
       expect(query.includes('orderDirection: desc')).toBeTruthy()
+    })
+  })
+})
+
+describe('#fromCollectionFragment', () => {
+  let collectionFragment: CollectionFragment
+
+  beforeEach(() => {
+    collectionFragment = {
+      id: 'id',
+      urn: 'urn',
+      name: 'name',
+      creator: 'creator',
+      searchIsStoreMinter: false,
+      itemsCount: 100,
+      createdAt: '100',
+      updatedAt: '200',
+      reviewedAt: '300',
+      firstListedAt: null,
+    }
+  })
+
+  describe('when fragment firstListedAt is null', () => {
+    beforeEach(() => {
+      collectionFragment.firstListedAt = null
+    })
+
+    it('should return a Collection with firstListedAt as null', () => {
+      const collection = fromCollectionFragment(
+        collectionFragment,
+        Network.MATIC,
+        ChainId.MATIC_MUMBAI
+      )
+
+      expect(collection.firstListedAt).toBeNull()
+    })
+  })
+
+  describe('when fragment firstListedAt is "100"', () => {
+    beforeEach(() => {
+      collectionFragment.firstListedAt = '100'
+    })
+
+    it('should return a Collection with firstListedAt as 100000', () => {
+      const collection = fromCollectionFragment(
+        collectionFragment,
+        Network.MATIC,
+        ChainId.MATIC_MUMBAI
+      )
+
+      expect(collection.firstListedAt).toBe(100000)
     })
   })
 })
