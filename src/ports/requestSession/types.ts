7848545c7e7a9caa847d76815bc5253311d0411a
Lautaro Petaccio <lausuper@gmail.com>
2023-02-28 11:57:44 -0500

    fix: Remove unused files

--- src/ports/requestSession/types.ts
+++
@@ -1,8 +0,0 @@
-import { IHttpServerComponent } from '@well-known-components/interfaces'
-import { Context } from '../../types'
-
-export type IRequestSessionComponent = {
-  createMiddleware: () => IHttpServerComponent.IRequestHandler<Context<string>>
-  getId: () => string
-  setId: (requestId: string) => void
-}
