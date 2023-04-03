9ba15df1f518c905eb521f84429837c3fe17e2f3
Kevin Szuchet <31735779+kevinszuchet@users.noreply.github.com>
2023-04-03 13:15:12 +0200

    feat: Allow filtering items by multiple ids (#260)
    * feat: Allow filtering items by multiple ids
    
    * fix: Check if the length of what getList returns is gt 0
    
    * chore: Upgrade schemas

--- src/adapters/handlers/items.ts
+++ src/adapters/handlers/items.ts
@@ -12,7 +12,7 @@ import {
 import { IHttpServerComponent } from '@well-known-components/interfaces'
 import { AppComponents, Context } from '../../types'
 import { Params } from '../../logic/http/params'
-import { asJSON } from '../../logic/http/response'
+import { asJSON, HttpError } from '../../logic/http/response'
 
 export function createItemsHandler(
   components: Pick<AppComponents, 'items'>
@@ -53,6 +53,7 @@ export function createItemsHandler(
       'emotePlayMode',
       EmotePlayMode
     )
+    const ids = params.getList('id')
     const contractAddresses = params.getList('contractAddress')
     const itemId = params.getString('itemId')
     const network = params.getValue<Network>('network', Network)
@@ -60,8 +61,18 @@ export function createItemsHandler(
     const minPrice = params.getString('minPrice')
     const urns = params.getList('urn')
 
-    return asJSON(() =>
-      items.fetchAndCount({
+    return asJSON(() => {
+      if (
+        ids?.length > 0 &&
+        (contractAddresses?.length > 0 || itemId || urns?.length > 0)
+      ) {
+        throw new HttpError(
+          'Ids cannot be set with contractAddress, itemId, or urn.',
+          400
+        )
+      }
+
+      return items.fetchAndCount({
         first,
         skip,
         sortBy,
@@ -78,6 +89,7 @@ export function createItemsHandler(
         emoteCategory,
         emoteGenders,
         emotePlayMode,
+        ids,
         contractAddresses,
         itemId,
         isWearableSmart,
@@ -90,6 +102,6 @@ export function createItemsHandler(
           : undefined,
         urns,
       })
-    )
+    })
   }
 }
