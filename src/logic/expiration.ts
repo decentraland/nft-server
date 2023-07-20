7dac6d3178607bc80deea069005fb289f9edb99c
Lautaro Petaccio <1120791+LautaroPetaccio@users.noreply.github.com>
2023-07-20 13:13:03 -0300

    fix: Expiration of orders in seconds (#326)
    * fix: Expiration of orders in seconds
    
    * feat: add logic to filter orders by expires_at
    
    * fix: test
    
    ---------
    
    Co-authored-by: Juanma Hidalgo <juanma06@gmail.com>

--- src/logic/expiration.ts
+++ src/logic/expiration.ts
@@ -1,3 +1,7 @@
 export function isExpired(expiresAt: string) {
   return +expiresAt < Date.now()
 }
+
+export function isOrderExpired(expiresAt: string) {
+  return BigInt(expiresAt) * 1000n < Date.now()
+}
