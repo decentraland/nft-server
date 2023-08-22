9d945734aef1a552a73859ab5d1b00bcd59e66b3
Juanma Hidalgo <juanma06@gmail.com>
2023-08-22 18:14:36 -0300

    test: add describes

--- src/tests/ports/catalog-utils.spec.ts
+++ src/tests/ports/catalog-utils.spec.ts
@@ -1,4 +1,4 @@
-import { ChainId, Network } from '@dcl/schemas'
+import { CatalogFilters, ChainId, Network } from '@dcl/schemas'
 import { getQuerySources } from '../../ports/catalog/utils'
 import { getSubgraphNameForNetwork } from '../../subgraphUtils'
 
@@ -11,40 +11,54 @@ jest.mock('../../logic/chainIds', () => ({
 }))
 
 describe('getQuerySources', () => {
-  it('should return sources for a specific network', () => {
-    const filters = { network: Network.ETHEREUM }
-    const sources = getQuerySources(filters)
-    expect(sources).toEqual({
-      [Network.ETHEREUM]: getSubgraphNameForNetwork(
-        Network.ETHEREUM,
-        marketplaceChainId as ChainId
-      ),
+  let filters: CatalogFilters
+  let sources: Record<string, string>
+  describe('when filtering by network', () => {
+    beforeEach(() => {
+      filters = { network: Network.ETHEREUM }
+      sources = getQuerySources(filters)
+    })
+    it('should return sources for the provided network', () => {
+      expect(sources).toEqual({
+        [Network.ETHEREUM]: getSubgraphNameForNetwork(
+          Network.ETHEREUM,
+          marketplaceChainId as ChainId
+        ),
+      })
     })
   })
 
-  it('should return sources for a specific creator with default networks', () => {
-    const filters = { creator: ['someCreator'] }
-    const sources = getQuerySources(filters)
-    expect(sources).toEqual({
-      [Network.MATIC]: getSubgraphNameForNetwork(
-        Network.MATIC,
-        collectionsChainId as ChainId
-      ),
+  describe('when filtering by creator', () => {
+    beforeEach(() => {
+      filters = { creator: ['someCreator'] }
+      sources = getQuerySources(filters)
+    })
+    it('should return sources for the creator provided with default networks', () => {
+      expect(sources).toEqual({
+        [Network.MATIC]: getSubgraphNameForNetwork(
+          Network.MATIC,
+          collectionsChainId as ChainId
+        ),
+      })
     })
   })
 
-  it('should return sources for default networks when no filter is provided', () => {
-    const filters = {}
-    const sources = getQuerySources(filters)
-    expect(sources).toEqual({
-      [Network.ETHEREUM]: getSubgraphNameForNetwork(
-        Network.ETHEREUM,
-        marketplaceChainId as ChainId
-      ),
-      [Network.MATIC]: getSubgraphNameForNetwork(
-        Network.MATIC,
-        collectionsChainId as ChainId
-      ),
+  describe('when no filter is provided', () => {
+    beforeEach(() => {
+      filters = {}
+      sources = getQuerySources(filters)
+    })
+    it('should return sources for default networks', () => {
+      expect(sources).toEqual({
+        [Network.ETHEREUM]: getSubgraphNameForNetwork(
+          Network.ETHEREUM,
+          marketplaceChainId as ChainId
+        ),
+        [Network.MATIC]: getSubgraphNameForNetwork(
+          Network.MATIC,
+          collectionsChainId as ChainId
+        ),
+      })
     })
   })
 })
